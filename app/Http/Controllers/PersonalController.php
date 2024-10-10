7f75d23b547b551534e84c574da4afcd47c259fc
Henrry <148930610+HenrryDG@users.noreply.github.com>
2024-10-10 12:21:37 -0400

    Adición del registro de personal y cambios en el diseño del dashboard

--- app/Http/Controllers/PersonalController.php
+++ app/Http/Controllers/PersonalController.php
@@ -21,10 +21,36 @@ class PersonalController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function insert(Request $request)
     {
-        //
+        // Crear una nueva instancia del modelo Usuario
+
+        
+
+        $usuario = new Usuario();
+        $usuario->nombre = $request->input('name'); // Campo 'name' del formulario
+        $usuario->ap_paterno = $request->input('ap_pa'); // Campo 'ap_pa' del formulario
+        $usuario->ap_materno = $request->input('ap_ma'); // Campo 'ap_ma' del formulario
+        $usuario->correo = $request->input('email'); // Campo 'email' del formulario
+        $usuario->telefono = $request->input('telefono'); // Campo 'telefono' del formulario
+        $usuario->carnet = $request->input('carnet'); // Campo 'carnet' del formulario
+
+        $pass = substr($usuario->nombre, 0, 1) . substr($usuario->ap_paterno, 0, 1) . substr($usuario->ap_materno, 0, 1) . substr($usuario->carnet, -4);
+        $usuario->password = bcrypt($pass); // Contraseña generada a partir de los datos del formulario
+        $usuario->save(); // Guardar en la tabla usuarios
+
+        // Crear una nueva instancia del modelo Personal
+        $personal = new Personal();
+        $personal->usuario_id = $usuario->id; // Asociar el ID del usuario creado
+        $personal->fecha_contrato = date('Y-m-d', strtotime($request->input('fecha_contratacion')));
+        $personal->turno = $request->input('turno'); // Campo 'turno' del formulario
+        $personal->cargo = $request->input('rol'); // Agregar el campo 'rol' si lo necesitas
+        $personal->save(); // Guardar en la tabla personal
+
+        // Redireccionar o devolver una respuesta
+        return redirect()->back()->with('success', 'Personal agregado correctamente.');
     }
+
 
     /**
      * Store a newly created resource in storage.
--- resources/views/components/formularios/form-personal.blade.php
+++ resources/views/components/formularios/form-personal.blade.php
@@ -16,74 +16,68 @@
                 </button>
             </div>
             <!-- Modal body -->
-            <form class="p-4 md:p-5">
+            <form method="POST" action="{{ route('personal.insert') }}" class="p-4 md:p-5">
+                @csrf
                 <div class="grid gap-4 mb-4 grid-cols-2">
                     <!-- Nombre -->
                     <div class="col-span-2">
                         <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre</label>
-                        <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Denilson Asis" required="">
+                        <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Denilson Asis" required>
                     </div>
                     <!-- Apellido Paterno -->
                     <div class="col-span-2 sm:col-span-1">
                         <label for="ap_pa" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Apellido Paterno</label>
-                        <input type="text" name="ap_pa" id="ap_pa" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Savedra" required="">
+                        <input type="text" name="ap_pa" id="ap_pa" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Savedra" required>
                     </div>
                     <!-- Apellido Materno -->
                     <div class="col-span-2 sm:col-span-1">
                         <label for="ap_ma" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Apellido Materno</label>
-                        <input type="text" name="ap_ma" id="ap_ma" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Mamani" required="">
+                        <input type="text" name="ap_ma" id="ap_ma" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Mamani" required>
                     </div>
                     <!-- Fecha de Contratacion -->
                     <div class="col-span-2">
-                        <label for="turno" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Fecha de Contratacion</label>
+                        <label for="fecha_contratacion" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Fecha de Contratación</label>
                         <div class="relative max-w-sm">
-                            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-                                </svg>
-                            </div>
-                            <input id="datepicker-autohide" datepicker datepicker-autohide type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Seleccione una fecha">
+                            <input id="datepicker-autohide" datepicker datepicker-autohide type="text" name="fecha_contratacion" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Seleccione una fecha">
                         </div>
                     </div>
                     <!-- Correo Electronico -->
                     <div class="col-span-2">
-                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Correo Electronico</label>
-                        <input type="email" pattern=".+@example\.com" email="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="denil@gmai.com" required="">
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Correo Electrónico</label>
+                        <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="denil@gmail.com" required>
                     </div>
                     <!-- Telefono -->
                     <div class="col-span-2 sm:col-span-1">
-                        <label for="telefono" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Telefono</label>
-                        <input type="tel" pattern="[0-9]{7}" name="telefono" id="telefono" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="72072764" required="">
+                        <label for="telefono" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Teléfono</label>
+                        <input type="tel" name="telefono" id="telefono" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="72072764" required>
                     </div>
                     <!-- Carnet de Identidad -->
                     <div class="col-span-2 sm:col-span-1">
-                        <label for="carnet" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">CI</label>
-                        <input type="text" name="carnet" id="carnet" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="12085546AE" required="">
+                        <label for="carnet" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Carnet de Identidad</label>
+                        <input type="text" name="carnet" id="carnet" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="12085546AE" required>
                     </div>
                     <!-- Rol -->
                     <div class="col-span-2 sm:col-span-1">
                         <label for="rol" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Rol</label>
-                        <select id="rol" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                        <select name="rol" id="rol" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                             <option selected="">Seleccione el rol</option>
-                            <option value="doctor">Doctor</option>
-                            <option value="enfermera">Enfermera</option>
+                            <option value="Doctor">Doctor</option>
+                            <option value="Enfermera">Enfermera</option>
+                            <option value="Enfermero">Enfermero</option>
                         </select>
                     </div>
                     <!-- Turno -->
                     <div class="col-span-2 sm:col-span-1">
                         <label for="turno" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Turno</label>
-                        <select id="turno" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                        <select name="turno" id="turno" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                             <option selected="">Seleccione el turno</option>
-                            <option value="manana">Mañana</option>
-                            <option value="tarde">Tarde</option>
-                            <option value="noche">Noche</option>
+                            <option value="Mañana">Mañana</option>
+                            <option value="Tarde">Tarde</option>
+                            <option value="Noche">Noche</option>
                         </select>
                     </div>
                 </div>
                 <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path>
-                    </svg>
                     Agregar
                 </button>
             </form>
