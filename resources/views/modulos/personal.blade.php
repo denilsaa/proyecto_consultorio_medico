2e8a283cce81ff1feb8a59d1439b68653861b804
Henrry <148930610+HenrryDG@users.noreply.github.com>
2024-10-15 13:34:53 -0400

    Cambio para el titulo del modal de personal

--- resources/views/modulos/personal.blade.php
+++ resources/views/modulos/personal.blade.php
@@ -1,4 +1,8 @@
 <x-componentes.app-layout-home titulo="Personal">
+    @php
+        $edit = "edit-crud-modal";
+        $new = "crud-modal";
+    @endphp
     <div class="relative overflow-x-auto shadow-2xl shadow-black dark:shadow-neutral-300 sm:rounded-lg ">
         <div class="relative overflow-x-auto shadow-2xl shadow-black dark:shadow-white sm:rounded-lg">
             <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -48,12 +52,12 @@
                         <!-- nuevo -->
                         <div class="ml-2">
                             <!-- Boton Modal -->
-                            <button data-modal-target="crud-modal" data-modal-toggle="crud-modal" class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-1.5 text-center me-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-500 dark:focus:ring-green-800" type="button">
+                            <button data-modal-target="{{$new}}" data-modal-toggle="{{$new}}" class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-1.5 text-center me-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-500 dark:focus:ring-green-800" type="button">
                                 Nuevo
                             </button>
                             <!-- Modal -->
                             <div>
-                                <x-formularios.form-personal></x-formularios.form-personal>
+                                <x-formularios.form-personal titulo="Nuevo Personal" modal="{{$new}}"/>
                             </div>
                         </div>
                     </div>
@@ -173,12 +177,18 @@
                         </td>
                         <!-- acciones -->
                         <td class="px-6 py-4">
-                            <button class="text-blue-400 hover:text-white border border-blue-400 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-300 dark:text-blue-300 dark:hover:text-white dark:hover:bg-blue-400 dark:focus:ring-blue-900">
+                            <button data-modal-target="{{$edit}}" data-modal-toggle="{{$edit}}" class="text-blue-400 hover:text-white border border-blue-400 hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-300 dark:text-blue-300 dark:hover:text-white dark:hover:bg-blue-400 dark:focus:ring-blue-900 transition-all duration-200">
                                 <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                     <path fill-rule="evenodd" d="M11.32 6.176H5c-1.105 0-2 .949-2 2.118v10.588C3 20.052 3.895 21 5 21h11c1.105 0 2-.948 2-2.118v-7.75l-3.914 4.144A2.46 2.46 0 0 1 12.81 16l-2.681.568c-1.75.37-3.292-1.263-2.942-3.115l.536-2.839c.097-.512.335-.983.684-1.352l2.914-3.086Z" clip-rule="evenodd" />
                                     <path fill-rule="evenodd" d="M19.846 4.318a2.148 2.148 0 0 0-.437-.692 2.014 2.014 0 0 0-.654-.463 1.92 1.92 0 0 0-1.544 0 2.014 2.014 0 0 0-.654.463l-.546.578 2.852 3.02.546-.579a2.14 2.14 0 0 0 .437-.692 2.244 2.244 0 0 0 0-1.635ZM17.45 8.721 14.597 5.7 9.82 10.76a.54.54 0 0 0-.137.27l-.536 2.84c-.07.37.239.696.588.622l2.682-.567a.492.492 0 0 0 .255-.145l4.778-5.06Z" clip-rule="evenodd" />
-                                </svg>
+                                </svg>        
                             </button>
+
+                            <!-- modal -->
+                            <div>
+                                <x-formularios.form-personal titulo="Editar Informacion" modal="{{$edit}}"/>
+                            </div>
+                            
                         </td>
                     </tr>
                     @endforeach
