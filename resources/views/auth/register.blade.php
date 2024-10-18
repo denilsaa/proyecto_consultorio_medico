1081db2a3887ee3f20d835b3e2f8f15170dd1bc1
Alejandro <cra2028252@est.univalle.edu>
2024-10-18 10:46:37 -0400

    Refactorizar modelo Usuario y vistas relacionadas

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,40 +2,59 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
-        <!-- Name -->
+        <!-- Nombre -->
         <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+            <x-input-label for="nombre" :value="__('Nombre')" />
+            <x-text-input id="nombre" class="block mt-1 w-full" type="text" name="nombre" :value="old('nombre')" required autofocus autocomplete="nombre" />
+            <x-input-error :messages="$errors->get('nombre')" class="mt-2" />
         </div>
 
-        <!-- Email Address -->
+        <!-- Apellido Paterno -->
         <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            <x-input-label for="ap_paterno" :value="__('Apellido Paterno')" />
+            <x-text-input id="ap_paterno" class="block mt-1 w-full" type="text" name="ap_paterno" :value="old('ap_paterno')" required autocomplete="ap_paterno" />
+            <x-input-error :messages="$errors->get('ap_paterno')" class="mt-2" />
         </div>
 
-        <!-- Password -->
+        <!-- Apellido Materno -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="ap_materno" :value="__('Apellido Materno')" />
+            <x-text-input id="ap_materno" class="block mt-1 w-full" type="text" name="ap_materno" :value="old('ap_materno')" required autocomplete="ap_materno" />
+            <x-input-error :messages="$errors->get('ap_materno')" class="mt-2" />
+        </div>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+        <!-- Correo -->
+        <div class="mt-4">
+            <x-input-label for="correo" :value="__('Correo')" />
+            <x-text-input id="correo" class="block mt-1 w-full" type="email" name="correo" :value="old('correo')" required autocomplete="correo" />
+            <x-input-error :messages="$errors->get('correo')" class="mt-2" />
+        </div>
 
+        <!-- Teléfono -->
+        <div class="mt-4">
+            <x-input-label for="telefono" :value="__('Teléfono')" />
+            <x-text-input id="telefono" class="block mt-1 w-full" type="text" name="telefono" :value="old('telefono')" required autocomplete="telefono" />
+            <x-input-error :messages="$errors->get('telefono')" class="mt-2" />
+        </div>
+
+        <!-- Carnet -->
+        <div class="mt-4">
+            <x-input-label for="carnet" :value="__('Carnet')" />
+            <x-text-input id="carnet" class="block mt-1 w-full" type="text" name="carnet" :value="old('carnet')" required autocomplete="carnet" />
+            <x-input-error :messages="$errors->get('carnet')" class="mt-2" />
+        </div>
+
+        <!-- Contraseña -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Contraseña')" />
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <!-- Confirm Password -->
+        <!-- Confirmar Contraseña -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
+            <x-input-label for="password_confirmation" :value="__('Confirmar Contraseña')" />
+            <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
