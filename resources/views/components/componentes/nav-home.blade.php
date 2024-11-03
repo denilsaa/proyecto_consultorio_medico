0b6991a9847ae706fb67a848cc2b28771d89a322
Alejandro <cra2028252@est.univalle.edu>
2024-11-02 23:00:15 -0400

    Refactor diseño de navegación y layout, actualizando estilos y reemplazando el logo SVG por una imagen.

--- resources/views/components/componentes/nav-home.blade.php
+++ resources/views/components/componentes/nav-home.blade.php
@@ -72,37 +72,6 @@
                     </div>
                 </div>
             </div>
-
-
-
-            
-
-        </div>
-    </div>
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
-        <div class="pt-4 pb-1 border-t border-gray-200">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
-            <div class="mt-3 space-y-1">
-                <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
-                </x-responsive-nav-link>
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-                    <x-responsive-nav-link href="/"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
-            </div>
         </div>
     </div>
 </nav>
