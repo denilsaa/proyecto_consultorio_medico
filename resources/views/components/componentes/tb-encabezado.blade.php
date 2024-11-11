c9a6ff577093520ade2fff839935c1f24c845eda
Henrry <148930610+HenrryDG@users.noreply.github.com>
2024-11-10 20:01:47 -0400

    Eliminación de espacios innecesarios en el componente 'tb-encabezado'

--- resources/views/components/componentes/tb-encabezado.blade.php
+++ resources/views/components/componentes/tb-encabezado.blade.php
@@ -78,8 +78,7 @@
                 <x-componentes.btn-new wire:click="$set('open',true)">
                     <svg class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
-                    </svg>
-                    
+                    </svg>                    
                 </x-componentes.btn-new>
             </div>
         </div>
